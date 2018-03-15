98412ebc325756b1961686c5909e175a0975fd6e
davidyannick <davidyannick@gmail.com>
2018-03-15 22:41:51 +0100

    tests fixed

--- src/test/java/me/wedding/dytwedding/repositories/WeddingRepositoryTest.java
+++ src/test/java/me/wedding/dytwedding/repositories/WeddingRepositoryTest.java
@@ -1,38 +1,41 @@
-//package me.wedding.dytwedding.repositories;
-//
-//import me.wedding.dytwedding.DatasForTest;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import reactor.core.publisher.Flux;
-//import reactor.test.StepVerifier;
-//
-//@ExtendWith(SpringExtension.class)
-//@DataMongoTest
-//class WeddingRepositoryTest {
-//
-//    @Autowired
-//    private WeddingRepository repository;
-//
-//    private DatasForTest datasForTest = new DatasForTest();
-//
-//    @BeforeEach
-//    void setUp() {
-//    }
-//
-//    @Test
-//    void findAll() {
-//        StepVerifier.create(repository.deleteAll()).verifyComplete();
-//
-//        StepVerifier.create(repository.saveAll(Flux.just(datasForTest.wedding1)))
-//                .expectNextCount(1L)
-//                .verifyComplete();
-//
-//        StepVerifier.create(repository.findAll())
-//                .expectNextCount(1L)
-//                .verifyComplete();
-//    }
-//}
+package me.wedding.dytwedding.repositories;
+
+import me.wedding.dytwedding.BootDatasTests;
+import me.wedding.dytwedding.domain.Wedding;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@ExtendWith(SpringExtension.class)
+@DataMongoTest
+class WeddingRepositoryTest {
+
+    @Autowired
+    private WeddingRepository repository;
+
+    private BootDatasTests bootDatasTests;
+
+
+    @BeforeEach
+    void setUp() {
+        bootDatasTests = new BootDatasTests();
+        Wedding wedding1 = bootDatasTests.getWedding1();
+        Wedding wedding2 = bootDatasTests.getWedding2();
+
+        StepVerifier.create(repository.deleteAll()).verifyComplete();
+
+        StepVerifier.create(Flux.just(wedding1, wedding2).flatMap(repository::save)).expectNextCount(2L).verifyComplete();
+    }
+
+    @Test
+    void findAll() {
+        StepVerifier.create(repository.findAll())
+                .expectNextCount(2L)
+                .verifyComplete();
+    }
+}
--- src/test/java/me/wedding/dytwedding/services/WeddingServiceTest.java
+++ src/test/java/me/wedding/dytwedding/services/WeddingServiceTest.java
@@ -1,38 +1,38 @@
-//package me.wedding.dytwedding.services;
-//
-//import me.wedding.dytwedding.DatasForTest;
-//import me.wedding.dytwedding.domain.Wedding;
-//import me.wedding.dytwedding.repositories.WeddingRepository;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import reactor.core.publisher.Flux;
-//import reactor.test.StepVerifier;
-//
-//@ExtendWith(SpringExtension.class)
-//class WeddingServiceTest {
-//
-//    @Mock
-//    private WeddingRepository weddingRepository;
-//
-//    @InjectMocks
-//    private WeddingServiceImpl weddingService;
-//
-//    private DatasForTest datasForTest = new DatasForTest();
-//
-//    @Test
-//    void getAllWeddings() {
-//        Mockito.when(weddingRepository.findAll()).thenReturn(Flux.just(datasForTest.wedding1));
-//
-//        Flux<Wedding> weddingFlux = weddingService.getAllWeddings();
-//
-//        StepVerifier.create(weddingFlux)
-//                .expectNextCount(1L)
-//                .verifyComplete();
-//
-//        Mockito.verify(weddingRepository).findAll();
-//    }
-//}
+package me.wedding.dytwedding.services;
+
+import me.wedding.dytwedding.BootDatasTests;
+import me.wedding.dytwedding.domain.Wedding;
+import me.wedding.dytwedding.repositories.WeddingRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@ExtendWith(SpringExtension.class)
+class WeddingServiceTest {
+
+    @Mock
+    private WeddingRepository weddingRepository;
+
+    @InjectMocks
+    private WeddingServiceImpl weddingService;
+
+    private BootDatasTests datasForTest = new BootDatasTests();
+
+    @Test
+    void getAllWeddings() {
+        Mockito.when(weddingRepository.findAll()).thenReturn(Flux.just(datasForTest.getWedding1(), datasForTest.getWedding2()));
+
+        Flux<Wedding> weddingFlux = weddingService.getAllWeddings();
+
+        StepVerifier.create(weddingFlux)
+                .expectNextCount(2L)
+                .verifyComplete();
+
+        Mockito.verify(weddingRepository).findAll();
+    }
+}
