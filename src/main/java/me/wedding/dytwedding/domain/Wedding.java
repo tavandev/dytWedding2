e9dbe5631f5b6152746d20d3d2b478e26f0c3e26
davidyannick <davidyannick@gmail.com>
2018-03-15 22:20:44 +0100

    Id added

--- src/main/java/me/wedding/dytwedding/domain/Wedding.java
+++ src/main/java/me/wedding/dytwedding/domain/Wedding.java
@@ -1,6 +1,7 @@
 package me.wedding.dytwedding.domain;
 
 import lombok.*;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
@@ -15,6 +16,7 @@ import java.util.List;
 @Data
 public class Wedding {
 
+    @Id
     private String weddingId;
 
     private Couple couple;
